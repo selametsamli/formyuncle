d5a1367ce99d3ea46b524eda69d6f0c3e2e6cf10
selametsamli <selamet96@gmail.com>
2019-04-22 13:22:37 +0300

    static dosyalar eklendi

--- formyuncle/urls.py
+++ formyuncle/urls.py
@@ -15,7 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+from posts.views import post_list
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('posts/', include('posts.urls')),
+    path('', post_list, name='post_list'),
+
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
